76de96dfcf4689b96727645d743fcab0915c8b90
Ghanshyam Patel <ghanshyam.patel@uniphore.com>
2024-02-29 20:47:19 -0800

    added cron scheduling

--- resetSC.js
+++ resetSC.js
@@ -1,11 +1,18 @@
 import { SoundcraftUI } from 'soundcraft-ui-connection';
 
-const mixerIP = "10.119.20.15";
-const showName = "test";
-const snapshotName = "snapTest";
+const mixerIP = "10.10.2.1";
+const showName = "test2";
+const snapshotName = "aarti";
+const cronTiming = "0 3 * * *"
 
 const conn = new SoundcraftUI(mixerIP);
 
-conn.connect();
-conn.shows.loadSnapshot(showName, snapshotName);
-conn.disconnect(); // close connection
+function changeShow(mixerIP, showName, snapshotName) {
+    conn.connect();
+    conn.shows.loadSnapshot(showName, snapshotName);
+    conn.disconnect(); // close connection
+}
+
+schedule.scheduleJob(cronTiming, () => { 
+    changeShow(mixerIP,showName,snapshotName);
+});
