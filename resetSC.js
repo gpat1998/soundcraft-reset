5f56103b735b24a28d1bae7f368bf96842b474e9
Ghanshyam Patel <ghanshyam.patel@uniphore.com>
2024-02-29 21:15:03 -0800

    dont need scheduler

--- resetSC.js
+++ resetSC.js
@@ -1,10 +1,8 @@
 import { SoundcraftUI } from 'soundcraft-ui-connection';
-import schedule from 'node-schedule';
 
 const mixerIP = "10.10.2.1";
 const showName = "test2";
 const snapshotName = "aarti";
-const cronTiming = "0 3 * * *"
 
 const conn = new SoundcraftUI(mixerIP);
 
@@ -14,6 +12,4 @@ function changeShow(mixerIP, showName, snapshotName) {
     conn.disconnect(); // close connection
 }
 
-schedule.scheduleJob(cronTiming, () => { 
-    changeShow(mixerIP,showName,snapshotName);
-});
+changeShow(mixerIP,showName,snapshotName);
